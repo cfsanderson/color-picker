eab69bc3305ce1a34184d8b755a899d136dda2c7
Caleb Frost Sanderson <calebsanderson@gmail.com>
2016-11-21 19:41:07 -0500

    finished with restyled rangers

--- public/main.js
+++ public/main.js
@@ -1,11 +1,3 @@
-// [x] Create sliders that serve as inputs for hue, saturation, lightness and values, e.g. <input type="range" name="hue">.
-
-// [x] Create a "swatch" element that will show the current color.
-
-// Show the current color as a ready to copy-and-paste CSS color function in hsl format, e.g. hsla(72, 90%, 80%, 0.53).
-
-// Respond to the correct events to update both the swatch and the color function to show current color when adjusting the sliders.
-
 let hue = 232
 let saturation = 7
 let lightness = 20
@@ -48,45 +40,3 @@ const main = () => {
 }
 
 document.addEventListener('DOMContentLoaded', main)
-
-// const displayFibbs = (event) => {
-//   if (event) event.preventDefault()
-//   const input = document.querySelector('input[name=n]')
-//   const n = parseInt(input.value)
-//
-//   const fibbs = fibbonacci(n)
-//
-//   const list = document.querySelector('ul')
-//
-//   while (list.lastChild) {
-//     list.removeChild(list.lastChild)
-//   }
-//
-//   for (let i = 0; i < fibbs.length; i++) {
-//     const li = document.createElement('li')
-//     li.textContent = fibbs[i]
-//
-//     const hue = Math.round((i / n) * 255)
-//
-//     li.style.backgroundColor = `hsl(${hue}, 100%, 50%)`
-//
-//     list.appendChild(li)
-//   }
-// }
-//
-// const fibbonacci = (n) => {
-//   let sequence = []
-//   let temp
-//   let a = 0
-//   let b = 1
-//
-//   while (n >= 0) {
-//     sequence.push(a)
-//     temp = a
-//     a = a + b
-//     b = temp
-//     n--
-//   }
-//
-//   return sequence
-// }
